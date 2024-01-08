b969de51813dd6bbb7152cccefaa2519ef7a98d8
Kỳ Lê <kylh84@gmail.com>
2024-01-08 17:24:03 +0700

    Add how are you msg

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Hello World! Goodbye!
+          How are you?
         </p>
         <a
           className="App-link"
