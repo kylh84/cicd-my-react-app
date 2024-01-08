d826789cb70452dd6ecaad612cd4fc22ed5e3df0
Kỳ Lê <kylh84@gmail.com>
2024-01-08 17:04:06 +0700

    Delete hihi

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Hello World!hi hi
+          Hello World!
         </p>
         <a
           className="App-link"
